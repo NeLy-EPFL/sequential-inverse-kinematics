e266999c774171ce61bd343dfe0e9b9fae465d8d
Gizem Ozdil <pembe.ozdil@epfl.ch>
2023-04-16 17:02:14 +0200

    modify grid plot to show both legs' joint angles

--- nmf_ik/visualization.py
+++ nmf_ik/visualization.py
@@ -370,22 +370,40 @@ def plot_trailing_kp(ax3d, points3d, key_points, export_path=None, t=0, trail=5)
         )
 
     if export_path is not None:
-        fig.savefig(export_path, bbox_inches="tight")
+        plt.savefig(export_path, bbox_inches="tight")
 
 
 def plot_joint_angle(
-    ax, kinematics_data, angles_to_plot, x_axis=None, degrees=True, fps=100, until_t=-1
+    ax: plt.Axes,
+    kinematics_data: Dict[str, np.ndarray],
+    angles_to_plot: List,
+    degrees: bool = True,
+    until_t: int = -1,
+    stim_lines: List[int] = None,
+    show_legend: bool = True,
+    export_path: Path = None
 ):
-    """[summary]
+    """Plot joint angles from a given kinematics data.
 
     Parameters
     ----------
-    kinematics_data : [type]
-        [description]
-    leg : str
-        Name of the leg. e.g., RF
-    ax : [type], optional
-        [description], by default None
+    ax : plt.Axes
+        Axis where the plot will be displayed.
+    kinematics_data : Dict[str, np.ndarray]
+        Dictionary containing the kienmatics, pose and angle
+    angles_to_plot : List
+        Angles to plot. Exact column name should be given.
+    degrees : bool, optional
+        Convert to degrees, by default True
+    until_t : int, optional
+        Plots the angles until t^th frame, by default -1
+    stim_lines : List[int], optional
+        Plots vertical lines in indicated locations,
+        by default None
+    show_legend : bool, optional
+        Shows legend, by default True
+    export_path : Path, optional
+        Path where the plot will be saved, by default None
     """
     colors = mcp.gen_color(cmap="Set2", n=len(angles_to_plot))
 
@@ -393,67 +411,157 @@ def plot_joint_angle(
         joint_angles = kinematics_data[joint_name]
 
         label = " ".join((joint_name.split("_")[-2], joint_name.split("_")[-1]))
-        if degrees:
-            ax.plot(
-                np.array(joint_angles[:until_t]) * 180 / np.pi,
-                "o-",
-                ms=5,
-                markevery=[-1],
-                label=label,
-                color=colors[i],
-            )
-        else:
-            ax.plot(
-                np.array(joint_angles[:until_t]),
-                "o-",
-                ms=5,
-                markevery=[-1],
-                label=label,
-                color=colors[i],
-            )
+
+        convert2deg = 180 / np.pi if degrees else 1
+
+        ax.plot(
+            np.array(joint_angles[:until_t]) * convert2deg,
+            "o-",
+            ms=4.5,
+            markevery=[-1],
+            label=label,
+            color=colors[i],
+        )
+
+    if stim_lines is not None:
+        ax.vlines(stim_lines, -200, 200, 'red', lw=0.5)
 
     ax.spines["top"].set_visible(False)
     ax.spines["right"].set_visible(False)
+    if show_legend:
+        ax.legend(bbox_to_anchor=(1.2, 1), frameon=False, borderaxespad=0.)
 
-    ax.legend(bbox_to_anchor=(1.2, 1), frameon=False, borderaxespad=0.)
+    if export_path is not None:
+        plt.savefig(export_path, bbox_inches="tight")
 
 
 def plot_grid(
-    img_path,
-    aligned_pose,
-    joint_angles,
-    leg_angles_to_plot,
-    head_angles_to_plot,
-    key_points_3d,
-    key_points_3d_trail,
-    t,
-    t_start,
-    t_end,
-    fps=100,
-    trail=30,
-    export_path=None,
+    img: np.ndarray,
+    aligned_pose: Dict[str, np.ndarray],
+    joint_angles: Dict[str, np.ndarray],
+    leg_angles_to_plot: List[str],
+    head_angles_to_plot: List[str],
+    key_points_3d: Dict[str, Tuple[np.ndarray, str]],
+    key_points_3d_trail: Dict[str, Tuple[np.ndarray, str]],
+    t: int,
+    t_start: int,
+    t_end: int,
+    t_interval: int = 20,
+    fps: int = 100,
+    trail: int = 30,
+    stim_lines: List[int] = None,
+    export_path: Path = None,
 ):
+    """
+    Plots an instance of the animal recording, 3D pose,
+    head and leg joint angles in a grid layout. This code
+    is intended to use in a for loop to plot and save all
+    the frames from `t_start` to `t_end` to make a video afterwards.
 
+    NOTE: This function is intended to plot leg and head joint angles together,
+    it will not work if any of these data is missing.
+
+    Parameters
+    ----------
+    img : Path
+        Image of the fly at frame t.
+    aligned_pose : Dict[str, np.ndarray]
+        Aligned 3D pose.
+    joint_angles : Dict[str, np.ndarray]
+        Joint angles.
+    leg_angles_to_plot : List[str]
+        List containing leg joint angle names without the side.
+        Example:
+            leg_joint_angles = [
+                "ThC_yaw",
+                "ThC_pitch",
+                "ThC_roll",
+                "CTr_pitch",
+                "CTr_roll",
+                "FTi_pitch",
+                "TiTa_pitch",
+                ]
+    head_angles_to_plot : List[str]
+        List containing exact names of head joint angle names.
+    key_points_3d : Dict[str, Tuple[np.ndarray, str]]
+        Dictionary mapping key points names to their indices
+        and line styles.
+        Example:
+            KEY_POINTS_DICT = {
+                "RF": (np.arange(0, 5), "solid"),
+                "R Ant": (np.arange(10, 12), "o"),
+                "Neck": (np.arange(14, 15), "x"),
+                "L Ant": (np.arange(12, 14), "o"),
+                "LF": (np.arange(5, 10), "solid"),
+            }
+    key_points_3d_trail : Dict[str, Tuple[np.ndarray, str]]
+        Dictionary mapping key points names to their indices
+        and line styles for trailing key points.
+    t : int
+        Frame number t.
+    t_start : int
+        Start of the time series, i.e., joint angles.
+    t_end : int
+        End of the time series, i.e., joint angles.
+    t_interval : int, optional
+        Interval of frame numbers in between x ticks, by default 20
+    fps : int, optional
+        Frames per second, by default 100
+    trail : int, optional
+        Number of previous frames where the key point will be visible,
+        by default 30
+    stim_lines : List[int], optional
+        Stimulation indicators, by default None
+    export_path : Path, optional
+        Path where the plot will be saved, by default None
+
+    Returns
+    -------
+    Fig
+        Figure.
+    """
+
+    assert t_start <= t <= t_end, "t_start should be smaller than t_end, t should be in between"
     # import pylustrator
     # pylustrator.start()
+
     plt.style.use("dark_background")
 
     fig = plt.figure(figsize=(14, 6), dpi=120)
 
     gs = GridSpec(3, 4, figure=fig)
-    ax_img = fig.add_subplot(gs[0, :])
+    # 7cam recording
+    ax_img = fig.add_subplot(gs[0, :1])
+    # 3D pose
     ax1 = fig.add_subplot(gs[1:, :2], projection="3d")
-    ax2 = fig.add_subplot(gs[1, 2:])
-    ax3 = fig.add_subplot(gs[2, 2:])
+    # head, right leg, left leg joint angles
+    ax2 = fig.add_subplot(gs[0, 2:])
+    ax3 = fig.add_subplot(gs[1, 2:])
+    ax4 = fig.add_subplot(gs[2, 2:])
 
     # load the image
-    img = cv2.imread(str(img_path / f'frame_{t}.jpg'), 0)
+
+    # img = cv2.imread(str(img_path / f'frame_{t}.jpg'), 0)
     ax_img.imshow(img, vmin=0, vmax=255, cmap='gray')
 
     plot_3d_points(ax1, aligned_pose, key_points=key_points_3d, t=t)
     plot_trailing_kp(ax1, aligned_pose, key_points=key_points_3d_trail, trail=trail, t=t)
-    plot_joint_angle(ax3, joint_angles, angles_to_plot=leg_angles_to_plot, until_t=t)
-    plot_joint_angle(ax2, joint_angles, angles_to_plot=head_angles_to_plot, until_t=t)
+    plot_joint_angle(ax2, joint_angles, angles_to_plot=head_angles_to_plot, until_t=t, stim_lines=stim_lines)
+    plot_joint_angle(
+        ax3,
+        joint_angles,
+        angles_to_plot=[
+            f"Angle_LF_{ja}" for ja in leg_angles_to_plot],
+        until_t=t,
+        stim_lines=stim_lines)
+    plot_joint_angle(
+        ax4,
+        joint_angles,
+        angles_to_plot=[
+            f"Angle_RF_{ja}" for ja in leg_angles_to_plot],
+        until_t=t,
+        show_legend=False,
+        stim_lines=stim_lines)
 
     ax_img.axis('off')
     # ax1 properties
@@ -468,42 +576,65 @@ def plot_grid(
 
     # ax2 properties
     ax2.set_xlim((t_start, t_end))
-    # ax2.set_ylabel("Head joint angles (deg)")
     ax2.spines["bottom"].set_visible(False)
-    ax2.set_xticks([])
     ax2.set_xlim((t_start, t_end))
-
-    ax2.set_ylim((-60, 60))
-    ax2.set_yticks(ticks=[-60, 0, 60])
-    ax2.set_yticklabels(labels=[-60, 0, 60])
+    ax2.set_ylim((-90, 70))
+    ax2.set_xticks([])
+    ax2.set_yticks(ticks=[-90, 0, 70])
+    ax2.set_yticklabels(labels=[-90, 0, 70])
 
     # ax3 properties
     ax3.set_xlim((t_start, t_end))
+    ax3.spines["bottom"].set_visible(False)
     ax3.set_ylim((-160, 160))
+    ax3.set_xticks([])
     ax3.set_yticks(ticks=[-160, 0, 160])
     ax3.set_yticklabels(labels=[-160, 0, 160])
 
-    ax3.set_xticks(ticks=np.arange(t_start, t_end + 50, 50))
-    ax3.set_xticklabels(labels=np.arange(t_start, t_end + 50, 50) / fps)
+    # ax4 properties
+    ax4.set_xlim((t_start, t_end))
+    ax4.set_ylim((-160, 160))
+    ax4.set_yticks(ticks=[-160, 0, 160])
+    ax4.set_yticklabels(labels=[-160, 0, 160])
 
-    # ax3.set_ylabel("Leg joint angles (deg)")
-    ax3.set_xlabel("Time (sec)")
+    ax4.set_xticks(ticks=np.arange(t_start, t_end + t_interval, t_interval))
+    ax4.set_xticklabels(labels=np.arange(t_start, t_end + t_interval, t_interval) / fps)
+    ax4.set_xlabel("Time (sec)")
 
-    # % start: automatic generated code from pylustrator
-    fig.ax_dict = {ax.get_label(): ax for ax in fig.axes}
-    getattr(fig, '_pylustrator_init', lambda: ...)()
-    fig.set_size_inches(22.870000 / 2.54, 15.130000 / 2.54, forward=True)
-    fig.axes[0].set(position=[0.2786, 0.664, 0.4316, 0.326])
-    fig.axes[1].set(position=[0.00265, 0.1157, 0.2995, 0.4525])
-    fig.axes[2].legend(loc=(0.9902, -0.06284), frameon=False)
-    fig.axes[2].set_position([0.342251, 0.376048, 0.523162, 0.221085])
-    fig.axes[2].yaxis.labelpad = -20.137022
-    fig.axes[3].legend(loc=(0.9902, -0.01824), frameon=False)
-    fig.axes[3].set(position=[0.5293, 0.08108, 0.3707, 0.2265])
-    fig.axes[3].set_position([0.342251, 0.082519, 0.523162, 0.221085])
-    fig.axes[3].yaxis.labelpad = -26.465817
-    fig.text(0.3378, 0.6156, 'Head joint angles (deg)', transform=fig.transFigure, )
-    fig.text(0.3378, 0.3281, 'Leg joint angles (deg)', transform=fig.transFigure, )
+    # #% start: automatic generated code from pylustrator
+    plt.figure(1).ax_dict = {ax.get_label(): ax for ax in plt.figure(1).axes}
+    getattr(plt.figure(1), '_pylustrator_init', lambda: ...)()
+    plt.figure(1).set_size_inches(24.520000 / 2.54, 11.450000 / 2.54, forward=True)
+
+    plt.figure(1).axes[0].text(
+        1.2502,
+        1.0174,
+        'Head joint angles (deg)',
+        transform=plt.figure(1).axes[0].transAxes,
+    )  # id=plt.figure(1).axes[0].texts[0].new
+    plt.figure(1).text(
+        0.5263,
+        0.6169,
+        'Left leg joint angles (deg)',
+        transform=plt.figure(1).transFigure,
+    )  # id=plt.figure(1).texts[0].new
+    plt.figure(1).text(
+        0.5263,
+        0.3458,
+        'Right leg joint angles (deg)',
+        transform=plt.figure(1).transFigure,
+    )  # id=plt.figure(1).texts[1].new
+
+    plt.figure(1).axes[0].set(position=[0.0385, 0.5848, 0.2853, 0.3084])
+    plt.figure(1).axes[1].set(position=[0.07833, 0.04041, 0.2226, 0.4813])
+    plt.figure(1).axes[2].legend(loc=(1.056, -0.03196), frameon=False)
+    plt.figure(1).axes[2].set(position=[0.4071, 0.6564, 0.397, 0.2135])
+    plt.figure(1).axes[3].set(position=[0.4071, 0.3835, 0.397, 0.2135])
+    plt.figure(1).axes[3].legend(loc=(1.056, -0.5187), frameon=False)
+    plt.figure(1).axes[4].set(position=[0.4071, 0.1107, 0.397, 0.2135])
+
+    plt.figure(1).texts[0].set_position([0.403832, 0.608496])
+    plt.figure(1).texts[1].set_position([0.403832, 0.352935])
     # % end: automatic generated code from pylustrator
 
     if export_path is not None:
