0af7fe8c775e556b3f23aed3df64c0ac0363e792
Gizem Ozdil <pembe.ozdil@epfl.ch>
2024-02-13 16:46:48 +0100

    modify examples, add both leg ik methods

--- examples/example_entire_pipeline.py
+++ examples/example_entire_pipeline.py
@@ -1,24 +1,26 @@
 """
     Runs the entire pipeline from pose alignment to joint angles on a path given by the user.
 
-    Example:
-    >>> python example_entire_pipeline.py -p /mnt/nas/GO/7cam/220413_aJO-GAL4xUAS-CsChr/Fly001
-    >>> python example_entire_pipeline.py -p ../data/anipose_220525_aJO_Fly001_001
+    Example usage:
+    >>> python example_entire_pipeline.py -p ../data/anipose_220525_aJO_Fly001_001 --plot
 """
 import logging
 from pathlib import Path
 import time
 import argparse
+import numpy as np
 
-from nmf_ik.alignment import AlignPose
-from nmf_ik.kinematic_chain import KinematicChain
-from nmf_ik.leg_inverse_kinematics import LegInverseKinematics
-from nmf_ik.head_inverse_kinematics import HeadInverseKinematics
-from nmf_ik.data import BOUNDS, INITIAL_ANGLES, NMF_TEMPLATE
-from nmf_ik.utils import save_file
+from seqikpy.alignment import AlignPose, convert_from_anipose_to_dict
+from seqikpy.kinematic_chain import KinematicChainSeq
+from seqikpy.leg_inverse_kinematics import LegInvKinSeq
+from seqikpy.head_inverse_kinematics import HeadInverseKinematics
+from seqikpy.data import BOUNDS, INITIAL_ANGLES, NMF_TEMPLATE, PTS2ALIGN
+from seqikpy.utils import save_file
 
-# Change the logging level here
-logging.basicConfig(level=logging.INFO, format=" %(asctime)s - %(levelname)s- %(message)s")
+logging.basicConfig(
+    format=" %(asctime)s - %(levelname)s- %(message)s",
+    handlers=[logging.StreamHandler()]
+)
 
 
 def parse_args():
@@ -31,7 +33,12 @@ def parse_args():
         "--path",
         type=str,
         default=None,
-        help="Path of pose files",
+        help="Path of 3D pose data.",
+    )
+    parser.add_argument(
+        "--plot",
+        action="store_true",
+        help="Plot the joint angles.",
     )
     return parser.parse_args()
 
@@ -45,56 +52,69 @@ if __name__ == "__main__":
 
     paths = Path(path_name).rglob("pose-3d")
 
-    # DATA_PATH = Path('../data/anipose/220525_aJO_Fly001_001/new-template')
-    # f_path = DATA_PATH / "pose3d.h5"
+    for data_path in paths:
 
-    for DATA_PATH in paths:
-
-        logging.info("Running code in %s", DATA_PATH)
+        logging.info("Running code in %s", data_path)
 
         start = time.time()
-
+        # Align the 3D pose
         align = AlignPose.from_file_path(
-            main_dir=DATA_PATH,
+            main_dir=data_path,
             file_name="pose3d.h5",
-            convert_dict=True,
+            legs_list=["RF", "LF"],
+            convert_func=convert_from_anipose_to_dict,
+            pts2align=PTS2ALIGN,
             include_claw=False,
             nmf_template=NMF_TEMPLATE,
+            log_level="INFO"
         )
 
-        aligned_pos = align.align_pose(export_path=DATA_PATH)
-
+        aligned_pos = align.align_pose(export_path=data_path)
+        # Compute the head joint angles
         class_hk = HeadInverseKinematics(
             aligned_pos=aligned_pos,
             nmf_template=NMF_TEMPLATE,
         )
-        head_joint_angles = class_hk.compute_head_angles(export_path=DATA_PATH)
-
-        class_seq_ik = LegInverseKinematics(
+        head_joint_angles = class_hk.compute_head_angles(
+            export_path=data_path,
+            compute_ant_angles=True
+        )
+        # Calculate the leg joint angles using the sequential IK
+        class_seq_ik = LegInvKinSeq(
             aligned_pos=aligned_pos,
-            kinematic_chain_class=KinematicChain(
+            kinematic_chain_class=KinematicChainSeq(
                 bounds_dof=BOUNDS,
+                legs_list=["RF", "LF"],
                 nmf_size=None,
             ),
             initial_angles=INITIAL_ANGLES
         )
-        leg_joint_angles, forward_kinematics = class_seq_ik.run_ik_and_fk(export_path=DATA_PATH)
+        leg_joint_angles, forward_kinematics = class_seq_ik.run_ik_and_fk(
+            export_path=data_path,
+            hide_progress_bar=False
+        )
 
         full_body_ik = {**head_joint_angles, **leg_joint_angles}
 
-        save_file(DATA_PATH / "body_joint_angles.pkl", full_body_ik)
+        save_file(data_path / "body_joint_angles.pkl", full_body_ik)
 
         end = time.time()
         total_time = (end - start) / 60.0
 
         print(f"Total time taken to execute the code: {total_time} mins")
 
-        # plot the joint angles
-        PLOT = True
-        if PLOT:
+        # Plot the joint angles
+        if args.plot:
             import matplotlib.pyplot as plt
+
+            time_step = 1e-2
+            time = time = np.arange(0, full_body_ik['Angle_head_roll'].shape[0], 1) * time_step
 
             for ja_name, ja_value in full_body_ik.items():
                 plt.plot(ja_value, label=ja_name, lw=2)
+
             plt.legend()
+            plt.xlabel("Time (s)")
+            plt.ylabel("Joint angles (rad)")
+            plt.title("Head and foreleg joint angles")
             plt.show()
--- examples/example_leg_inv_kinematics.py
+++ examples/example_leg_inv_kinematics.py
@@ -1,35 +1,78 @@
 """ Example usage of leg inverse kinematics module. """
-import pickle
-from pathlib import Path
 import time
+from pathlib import Path
 
-from seqikpy.kinematic_chain import KinematicChain
-from seqikpy.leg_inverse_kinematics import LegInverseKinematics
-from seqikpy.data import BOUNDS, INITIAL_ANGLES, NMF_TEMPLATE
+import matplotlib.pyplot as plt
+
+from seqikpy.kinematic_chain import KinematicChainSeq, KinematicChainGeneric
+from seqikpy.leg_inverse_kinematics import LegInvKinSeq, LegInvKinGeneric
+from seqikpy.data import BOUNDS, INITIAL_ANGLES
+from seqikpy.utils import load_file
 
 
 DATA_PATH = Path('../data/anipose_220525_aJO_Fly001_001/pose-3d')
 
-start = time.time()
 
 f_path = DATA_PATH / "pose3d_aligned.pkl"
 
-with open(f_path, "rb") as f:
-    aligned_pos = pickle.load(f)
+aligned_pos = load_file(f_path)
 
+start = time.time()
 
-seq_ik = LegInverseKinematics(
+# Sequential inverse kinematics
+seq_ik = LegInvKinSeq(
     aligned_pos=aligned_pos,
-    kinematic_chain_class=KinematicChain(
+    kinematic_chain_class=KinematicChainSeq(
         bounds_dof=BOUNDS,
+        legs_list=["RF"],
         nmf_size=None,
     ),
     initial_angles=INITIAL_ANGLES
 )
 
-leg_joint_angles, forward_kinematics = seq_ik.run_ik_and_fk(export_path=DATA_PATH)
+leg_joint_angles_seq, forward_kinematics_seq = seq_ik.run_ik_and_fk(
+    export_path=DATA_PATH,
+    hide_progress_bar=True
+)
 
 end = time.time()
 total_time = (end - start) / 60.0
 
-print(f'Total time taken to execute the code: {total_time} mins')
+print(f'Sequential IK took {total_time} mins')
+
+start = time.time()
+
+# Traditional inverse kinematics
+gen_ik = LegInvKinGeneric(
+    aligned_pos=aligned_pos,
+    kinematic_chain_class=KinematicChainGeneric(
+        bounds_dof=BOUNDS,
+        legs_list=["RF"],
+        nmf_size=None,
+    ),
+    initial_angles=INITIAL_ANGLES
+)
+
+leg_joint_angles_gen, forward_kinematics_gen = gen_ik.run_ik_and_fk(
+    export_path=DATA_PATH,
+    hide_progress_bar=True
+)
+
+end = time.time()
+total_time = (end - start) / 60.0
+
+print(f'Traditional IK took {total_time} mins')
+
+# Compare the joint angles
+fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+for key in leg_joint_angles_seq:
+    plt.plot(leg_joint_angles_seq[key], label=key[6:], lw=2)
+    plt.plot(leg_joint_angles_gen[key], ls=':', lw=2)
+
+
+plt.xlabel('Frames (AU)')
+plt.ylabel('Angles (rad)')
+plt.title('Leg joint angles from SeqIK (solid) and GenIK (dotted)')
+plt.legend()
+plt.grid(True)
+plt.show()
